9c410697ab31527c95f01de20080cc6de9f000ce
Mael Brocher <35853844+MaelBrocher@users.noreply.github.com>
2021-12-06 11:54:25 +0100

    day 3 & 4

--- Day3/code_day_3.py
+++ Day3/code_day_3.py
@@ -1,5 +1,33 @@
+def power_comsumption(data, N):
+    bits = [2 ** n for n in range(N)]
+    gamma = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit >= len(data) / 2)
+    epsilon = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit <= len(data) / 2)
+    print("Part 1 = {}".format(epsilon * gamma))
+
+
+def filter_data_bitwise(data, N,filter_by_most_common=True):
+    filtered = [x for x in data]
+    bits = [2 ** n for n in range(N)]
+    for bit in reversed(bits):
+        ratio = sum(1 for num in filtered if num & bit) / len(filtered)
+        wanted_bit_value = bit * int((ratio >= 0.5) == filter_by_most_common)
+        filtered = [x for x in filtered if x & bit == wanted_bit_value]
+        if len(filtered) == 1:
+            break
+    return filtered
+
+def life_support(data, N):
+    oxygen = filter_data_bitwise(data, N)
+    co2 = filter_data_bitwise(data,N ,filter_by_most_common=False)
+    print("Part 2 = {}".format(oxygen[0] * co2[0]))
+
 def main():
-    return
+    with open("input.sql", "r") as f:
+        lines = f.readlines()
+    N = len(lines[0].strip())
+    data = [int(line, base=2) for line in lines]
+    power_comsumption(data, N)
+    life_support(data, N)
 
 if __name__ == '__main__':
     main()
--- Day4/code_day_4.py
+++ Day4/code_day_4.py
@@ -1,4 +1,26 @@
+from itertools import chain
+import sys
+
+def compute(boards, numbers, lastboard=False):
+    drawn, remaining = set(), set(range(len(boards)))
+    for number in map(int, numbers.split(",")):
+        drawn.add(number)
+        for i in set(remaining):
+            if any(line <= drawn for line in boards[i]):
+                remaining.remove(i)
+                if len(remaining) == len(boards) - 1 or not remaining:
+                    print(number * sum(set.union(*boards[i]) - drawn))
+
 def main():
+    numbers, *data = open("input.sql", "r").read().strip().split("\n\n")
+
+    boards = []
+    for board in data:
+        rows = [
+            [int(x) for x in row.split()] for row in board.split("\n")
+        ]
+        boards.append([set(line) for line in chain(rows, zip(*rows))])
+    compute(boards, numbers)
     return
 
 if __name__ == '__main__':

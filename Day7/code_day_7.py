257fbec17a93d9fa48fb85c7dd1f4651bd85e0c9
Mael Brocher <35853844+MaelBrocher@users.noreply.github.com>
2021-12-07 12:34:59 +0100

    best ugly and efficient oneliner ever

--- Day7/code_day_7.py
+++ Day7/code_day_7.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 def main():
-    return
+    crabs = [int(crab) for crab in open("input.sql", "r").readline().split(',')]
+
+    print("Part 1: {}".format(int(sum(abs(crab - np.median(crabs)) for crab in crabs))))
+
+    print("Part 2: {}".format(int(sum(abs(crab - (round(np.mean(crabs))-1)) * (abs(crab - (round(np.mean(crabs))-1))+1)//2 for crab in crabs))))
 
 if __name__ == '__main__':
     main()

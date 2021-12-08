736cc4b36aa3506834759e040ad7ec85f5348c01
Mael Brocher <35853844+MaelBrocher@users.noreply.github.com>
2021-12-08 14:43:20 +0100

    I like this more, still ugly tho

--- Day8/code_day_8.py
+++ Day8/code_day_8.py
@@ -1,20 +1,28 @@
 def part2(line):
     input = line[:10]
+    output = line[10:]
     decode_number = [None] * 10
-    #easy part
+    #easy but ugly part
+    #https://imgur.com/a/LIS2zZr
     for i, wanted in zip([1,4,7,8], [2,4,3,7]):
         decode_number[i] = ["".join(sorted(w)) for w in input if len(w) == wanted][0]
-
-    #kind of want to die
-    decode_number[3] = ["".join(sorted(w)) for w in input if len(w) == 5 and len(set(decode_number[7]) & set(w)) == 3][0]
-    decode_number[9] = ["".join(sorted(w)) for w in input if len(w) == 6 and set(decode_number[3]) < set(w)][0]
-    decode_number[5] = ["".join(sorted(w)) for w in input if len(w) == 5 and set(w) != set(decode_number[3]) and set(w) < set(decode_number[9])][0]
-    decode_number[2] = ["".join(sorted(w)) for w in input if len(w) == 5 and set(w) != set(decode_number[3]) and set(w) != set(decode_number[5])][0]
-    decode_number[6] = ["".join(sorted(w)) for w in input if len(w) == 6 and set(w) != set(decode_number[9]) and set(decode_number[5]) < set(w)][0]
-    decode_number[0] = ["".join(sorted(w)) for w in input if len(w) == 6 and set(w) != set(decode_number[6]) and set(w) != set(decode_number[9])][0]
-
+    for l in input:
+        if len(l) == 5:
+            if len(''.join(set(decode_number[7]).intersection(l))) == len(decode_number[7]):
+                decode_number[3] = "".join(sorted(l))
+            elif len(''.join(set(decode_number[4]).intersection(l))) == 3:
+                decode_number[5] = "".join(sorted(l))
+            else:
+                decode_number[2] = "".join(sorted(l))
+        elif len(l) == 6:
+            if len(''.join(set(decode_number[4]).intersection(l))) == len(decode_number[4]):
+                decode_number[9] = "".join(sorted(l))
+            elif len(''.join(set(decode_number[7]).intersection(l))) == len(decode_number[7]):
+                decode_number[0] = "".join(sorted(l))
+            else:
+                decode_number[6] = "".join(sorted(l))
     #I like this
-    return int("".join(str(decode_number.index("".join(sorted(w)))) for w in line[-4:]))
+    return int("".join(str(decode_number.index("".join(sorted(w)))) for w in output))
 
 def main():
     input = [[n for n in line.replace(' | ', ' ').strip().split(' ')] for line in open('input.sql','r').readlines()]

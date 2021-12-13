a28171a85561aacdaab9bc659384551447420872
Mael Brocher <mael.brocher@epitech.eu>
2021-12-13 19:31:43 +0100

    Update code_day_6.py

--- Day6/code_day_6.py
+++ Day6/code_day_6.py
@@ -5,7 +5,7 @@ def fishlivesmatter(fishes, time):
     return sum(fishes)
 
 def main():
-    fishes = [open('inputs/input6', 'r').readline().strip().split(',').count(str(i)) for i in range(8)]
+    fishes = [open('input.sql', 'r').readline().strip().split(',').count(str(i)) for i in range(9)]
 
     print("Part1 = {}".format(fishlivesmatter(fishes.copy(), 80)))
     print("Part2 = {}".format(fishlivesmatter(fishes.copy(), 256)))
